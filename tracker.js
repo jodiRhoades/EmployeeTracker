3fe2f7c5d6abb1395d20aebea2648e7923e4d4c1
Jodi Rhoades <jo_kawgirl@yahoo.com>
2020-01-15 21:23:21 -0500

    updated more functions in the department table

--- tracker.js
+++ tracker.js
@@ -28,83 +28,77 @@ function start() {
         name: "editORview",
         type: "list",
         message: "Would you like to add, view, or update a Department, Role or an Employee?",
-        choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "EXIT"] //GETS STUCK HERE
+        choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "EXIT"]
       }
-    ])    
+    ])
     .then(function (answer) {
       // based on their answer, either call the department. role or employee functions or exit
-      if (answer.editORview === "DEPATMENT") {
+      if (answer.editORview === "DEPARTMENT") {
         editDepartment();
-        // ------DO I start completely seperate function for the editDept for each answer or are they nested into editORview department answer?
-
-        function editDepartment() {
-          inquirer
-            .prompt([
-              {
-                name: "editDept",
-                type: "list",
-                message: "Would you like to [ADD], [VIEW], or [UPDATE] a Department?",
-                choices: ["ADD", "VIEW", "UPDATE", "EXIT"]
-              }
-            ])
-            .then(function (answer) {
-              // based on their answer, either call the ADD, VIEW or UPDATE functions for Department
-              if (answer.editDept === "ADD") {
-                addDept();
-                function addDept() {
-                  //ask for new department name
-                  inquirer
-                    .prompt([
-                      {
-                        name: "newDept",
-                        type: "editor",
-                        message: "What's the name of the New Department you would like to add?"
-                      },
-                    ])
-                    .then(function (answer) {
-                      // when finished prompting, insert a new department into the db with that info
-                      connection.query(
-                        "INSERT INTO department SET ?",
-                        {
-                          name: answer.name
-                        },
-                        function (err) {
-                          if (err) throw err;
-                          console.log("Congratulations, your new department" + answer.name + "was added.");
-                          start();
-                        }
-                      )
-                    });
-                }
-              }
-              else if (answer.editDept === "VIEW") {
-                viewDept();
-                function viewDept() {
-                  "SELECT * FROM department",
-                    //is this how I do this console.log?
-                    console.log();
-                  start();
-                }
-              }
-              else if (answer.editDept === "UPDATE") {
-                UPDATE department,
-                SET "name",
-                WHERE id = ?,
-
-                updateDept();
-                function updateDept() {
-                                   
-                }
-              }
-              else if (answer.editDept === "EXIT") {
-                connection.end();
-                start();
-              }
-            });
-        }
+      }
+      else if (answer.editORview === "ROLE") {
+        editRole();
+      }
+      else if (answer.editORview === "Employee") {
+        editEmployee();
+      } else {
+        connection.end();
       }
     });
 }
+
+function editDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "editDept",
+        type: "list",
+        message: "Would you like to [ADD], [VIEW], or [UPDATE] a Department?",
+        choices: ["ADD", "VIEW", "UPDATE", "EXIT"]
+      }
+    ])
+    .then(function (answer) {
+      // based on their answer, either call the ADD, VIEW or UPDATE functions for Department
+      if (answer.editDept === "ADD") {
+        addDept();
+      }
+    });
+}
+
+function addDept() {
+  //ask for new department name
+  inquirer
+    .prompt([
+      {
+        name: "newDept",
+        type: "input",
+        message: "What's the name of the New Department you would like to add?"
+      },
+    ])
+    .then(function (answer) {
+      // when finished prompting, insert a new department into the db with that info
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: answer.newDept,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Congratulations, your new department " + answer.newDept + " was added.");
+          start();
+        }
+      )
+    });
+}
+function viewDept() {  
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+//-----------------Placeholder---------------------------//
 
 
 
@@ -160,7 +154,6 @@ function start() {
 
 
 
-//-----------------Placeholder---------------------------//
 
 
       //-----------------------------------HALF OF OTHER FUNTION-------
@@ -177,4 +170,5 @@ function start() {
         } else{
           connection.end();
         }
-      });
+
+      });*/
